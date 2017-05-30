dfd82c9e8e21b76da5b8e7a090a05c07e84e6cfb
tnli <tnli@iki.fi>
2017-05-30 19:46:10 +0300

    testing controllers

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -3,12 +3,13 @@
   class HelloWorldController extends BaseController{
 
     public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+   	    View::make('home.html');
     }
 
     public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Tämä on etusivu';
+        // Testaa koodiasi täällä
+        // echo 'Tämä on etusivu';
+        View::make('helloworld.html');
     }
   }
